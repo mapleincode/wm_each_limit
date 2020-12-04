9d0410feed64d5491bfa1a2199be6309865d22d0
mapleincode <llkj13579@126.com>
2020-12-04 15:27:01 +0800

    feat: push

--- index.js
+++ index.js
@@ -1,59 +1,71 @@
-'use strict';
+/**
+ * @Author: maple
+ * @Date: 2020-12-04 13:33:31
+ * @LastEditors: maple
+ * @LastEditTime: 2020-12-04 15:14:54
+ */
 
-function eachLimit(items, limit, func) {
-    return new Promise(function(resolve, reject) {
-        let startIndex = 0;
-        const maxIndex = items.length;
-        let finishIndex = 0;
-        const results = [];
-        let resultStatus = true;
-        let error = null;
+async function eachLimit (items, limit, func, options = {}) {
+  const noerror = options.noerror || false;
 
-        const createFuncAndRun = function(status) {
-            if (!resultStatus) {
-                // 结束
-                return;
-            }
-            if (!status) finishIndex ++;
+  return new Promise(function (resolve, reject) {
+    let startIndex = 0;
+    const maxIndex = items.length;
+    let finishIndex = 0;
+    const results = [];
 
-            if (startIndex <= maxIndex) {
-                startIndex ++;
-                const item = items[startIndex - 1];
-                beginNewFunc(item);
-            }
-        };
+    let resultStatus = true;
+    let error = null;
 
-        const check = function() {
-            if(resultStatus && error) {
-                resultStatus = false;
-                return reject(error);
-            }
-            if(resultStatus && finishIndex >= maxIndex) {
-                resultStatus = false;
-                return resolve(results);
-            }
-        };
+    const createFuncAndRun = function (status) {
+      if (!resultStatus) {
+        // 结束
+        return;
+      }
+      if (!status) finishIndex++;
 
-        const beginNewFunc = async function(item) {
-            let result;
-            try {
-                result = await func(item);
-            } catch(err) {
-                error = err;
-            }
-            // push result
-            results.push(result);
+      if (startIndex <= maxIndex) {
+        startIndex++;
+        const item = items[startIndex - 1];
+        beginNewFunc(item);
+      }
+    };
 
-            check();
+    const check = function () {
+      if (resultStatus && error & noerror & !noerror) {
+        resultStatus = false;
+        return reject(error);
+      }
+      if (resultStatus && finishIndex >= maxIndex) {
+        resultStatus = false;
+        return resolve(results);
+      }
+    };
 
-            createFuncAndRun();
-        }
+    const beginNewFunc = async function (item) {
+      let result;
+      try {
+        result = await func(item);
+      } catch (err) {
+        error = err;
+      }
+      // push result
+      results.push(result);
 
-        let _limit = limit;
-        while(limit --) {
-            createFuncAndRun(true);
-        }
-    });
+      check();
+
+      createFuncAndRun();
+    };
+
+    /**
+     * create function run
+     */
+    let _limit = limit;
+    while (_limit > 0) {
+      _limit--;
+      createFuncAndRun(true);
+    }
+  });
 }
 
 module.exports = eachLimit;
