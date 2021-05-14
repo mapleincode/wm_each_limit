997703e337abae3df23a267236e7b381a313b618
mapleincode <llkj13579@126.com>
2021-05-14 17:09:03 +0800

    docs: add docs

--- index.js
+++ index.js
@@ -2,73 +2,17 @@
  * @Author: maple
  * @Date: 2020-12-04 13:33:31
  * @LastEditors: maple
- * @LastEditTime: 2021-05-14 16:41:25
+ * @LastEditTime: 2021-05-14 17:07:53
  */
 
-// async function eachLimit (items, limit, func, options = {}) {
-//   const noerror = options.noerror || false;
-
-//   return new Promise(function (resolve, reject) {
-//     let startIndex = 0;
-//     const maxIndex = items.length;
-//     let finishIndex = 0;
-//     const results = [];
-
-//     let resultStatus = true;
-//     let error = null;
-
-//     const createFuncAndRun = function (status) {
-//       if (!resultStatus) {
-//         // 结束
-//         return;
-//       }
-//       // if (!status) finishIndex++;
-
-//       if (startIndex < maxIndex) {
-//         startIndex++;
-//         const item = items[startIndex - 1];
-//         beginNewFunc(item);
-//       }
-//     };
-
-//     const check = function () {
-//       if (resultStatus && error & noerror & !noerror) {
-//         resultStatus = false;
-//         return reject(error);
-//       }
-//       if (resultStatus && finishIndex >= maxIndex) {
-//         resultStatus = false;
-//         return resolve(results);
-//       }
-//     };
-
-//     const beginNewFunc = async function (item) {
-//       let result;
-//       try {
-//         result = await func(item);
-//       } catch (err) {
-//         error = err;
-//       }
-//       finishIndex ++;
-//       // push result
-//       results.push(result);
-
-//       check();
-
-//       createFuncAndRun();
-//     };
-
-//     /**
-//      * create function run
-//      */
-//     let _limit = limit;
-//     while (_limit > 0) {
-//       _limit--;
-//       createFuncAndRun(true);
-//     }
-//   });
-// }
-
+/**
+ * eachLimit
+ * @param {Array} items items
+ * @param {Number} limit limit at one time
+ * @param {function} func Func
+ * @param {object} options options
+ * @returns Promise<result[]>
+ */
 const eachLimit = function (items, limit, func, options = {}) {
   return new Promise((resolve, reject) => {
     const noerror = options.noerror || false;
